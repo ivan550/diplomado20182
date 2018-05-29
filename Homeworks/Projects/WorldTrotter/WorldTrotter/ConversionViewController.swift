90b0511c79d752a62d887334015243e5db1f6333
IvánMS <IvanMS@IPNSVR2014BJV.local>
2018-05-29 01:41:33 -0500

    Until chapter 5

--- Homeworks/Projects/WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ Homeworks/Projects/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -11,6 +11,7 @@ class ConversionViewController: UIViewController,UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ConversionViewController loaded its view.")
         updateCelsiusLabel()
     }
     
---
+++ Homeworks/Projects/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by IvánMS on 29/05/18.
+//  Copyright © 2018 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+class MapViewController: UIViewController{
+    override func viewDidLoad() {
+        print("MapViewController loaded its view.")
+    }
+}
